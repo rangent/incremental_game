3a2c3255187d8f4c635246dd852a5c03d9eb6467
rangent <rangent0@yahoo.com>
2014-09-07 23:36:07 -0700

    Travel left and right buttons

--- js/debug.js
+++ js/debug.js
@@ -1,9 +1,17 @@
 function ftest1() {
 	var time = 0;
+	//initial explore
 	for (var i = 0; i < 10; i++) {
-		sleep((DEBUG) ? time += 100 : time += 100,testDoExplore);
+		sleep((DEBUG) ? time += 20 : time += 100,testDoExplore);
 	}
+
+	//initial forage
 	sleep((DEBUG) ? time += 1000 : time += 1000, testDoForage);
+
+	//get us some lands
+	for (var i = 0; i < 10; i++) {
+		sleep((DEBUG) ? time += 20 : time += 100,testDoExplore);
+	}
 
 }
 
--- js/views/travel.js
+++ js/views/travel.js
@@ -5,15 +5,35 @@
 function doTravel() {
 	var selectedTerrainId = $("#availableTerrain").select2("val");
 	if (selectedTerrainId != "" && selectedTerrainId != player.currentTerrain) {
-		if (typeof player.availableTerrain[player.currentTerrain] === "object") {
-			player.availableTerrain[player.currentTerrain].disabled = false;
-			player.availableTerrain[player.currentTerrain].text = player.availableTerrain[player.currentTerrain].text.substring(2);
-		}
+		doTravelToLocation(parseInt(selectedTerrainId));
+	}
+}
 
-		travelToLocation(selectedTerrainId);
-		updateCurrentTerrain();
-		player.availableTerrain[player.currentTerrain].text = "* " + player.availableTerrain[player.currentTerrain].text;
-		$("#availableTerrain").select2("val", "");
-		$("#selectedTerrain").empty();
+function doTravelToLocation(selectedTerrainIdToTravelTo) {
+	//logic to "enable" currently selected drop down item if it wasn't the  "select travel destination" item
+	//also remove the "* " indicating the player's current location
+	if (typeof player.availableTerrain[player.currentTerrain] === "object") {
+		player.availableTerrain[player.currentTerrain].disabled = false;
+		player.availableTerrain[player.currentTerrain].text = player.availableTerrain[player.currentTerrain].text.substring(2);
+	}
+
+	travelToLocation(selectedTerrainIdToTravelTo);
+	updateCurrentTerrain();
+	player.availableTerrain[player.currentTerrain].text = "* " + player.availableTerrain[player.currentTerrain].text;
+	$("#availableTerrain").select2("val", "");
+	$("#selectedTerrain").empty();
+}
+
+function travelLeft() {
+	if ((player.currentTerrain - 1) >= 0  && 
+		typeof player.availableTerrain[player.currentTerrain - 1] === "object") {
+		doTravelToLocation(parseInt(player.currentTerrain) - 1);
+	}
+}
+
+function travelRight() {
+	if ((player.currentTerrain + 1) < (player.availableTerrain.length)  && 
+		typeof player.availableTerrain[player.currentTerrain + 1] === "object") {
+		doTravelToLocation(parseInt(player.currentTerrain) + 1);
 	}
 }
