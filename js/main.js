5acda3031ba1af34dcf11753b53ae8e60ee38646
rangent <besserlieu@salesforce.com>
2014-11-12 20:27:56 -0800

    Can now move around the map with key presses

--- js/main.js
+++ js/main.js
@@ -21,6 +21,37 @@ function initializePlayerTerrainAsset() {
 	}
 }
 
+$(document).keydown(function(e) {
+    switch(e.which) {
+        case 37: // left
+			if (isTerrainTraversable(new Location(-1,0))) {
+				travelRelative(-1, 0);
+			}
+        break;
+
+        case 38: // up
+			if (isTerrainTraversable(new Location(0,-1))) {
+				travelRelative(0, -1);
+			}
+        break;
+
+        case 39: // right
+			if (isTerrainTraversable(new Location(1,0))) {
+				travelRelative(1, 0);
+			}
+        break;
+
+        case 40: // down
+			if (isTerrainTraversable(new Location(0,1))) {
+				travelRelative(0, 1);
+			}
+        break;
+
+        default: return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+});
+
 //////////////////////////////////////////////////////////////////////////////
 // INCREMENTAL LOOP
 //////////////////////////////////////////////////////////////////////////////
