d3fb2f3da10c86249273d66049e85c079da8ee8f
rangent <besserlieu@salesforce.com>
2015-07-07 23:54:47 -0700

    Height auto resizes when changing screen

--- js/scratch-bootstrap.js
+++ js/scratch-bootstrap.js
@@ -1,25 +1,29 @@
 var windowSize = -1;
 
-
-
 $( document ).ready(function() {
     var windowSize = getWindowSize();
     var w = $("#nwbutton").width();
     $("button").addClass("btn-" + windowSize);
     $(".direction-button").width(w);
-    
+    $("#maincol").height($(window).height() - $("footer").height() - $("header").height());
+    $("#mainrow").height($(window).height() - $("footer").height() - $("header").height());
 });
 
 function getWindowSize() {
     var w = $(window).width();
     /*if (w >= 1200) {
         return 'lg';
-    } else if (w >= 980) {
+    } else*/ if (w >= 980) {
         return 'md';
-    } else*/ if (w >= 768) {
+    } else if (w >= 768) {
         return 'sm';
     } else {
         return 'xs';
     }
 }
+
+$( window ).resize(function() {
+    $("#maincol").height($(window).height() - $("footer").height() - $("header").height());
+    $("#mainrow").height($(window).height() - $("footer").height() - $("header").height());
+});
 
