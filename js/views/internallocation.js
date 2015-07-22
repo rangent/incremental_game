5050857e9ab228bcd0c5cd82ccdff093fbeea74b
rangent <besserlieu@salesforce.com>
2015-07-21 21:46:45 -0700

    Cytoscape now just redraws the nodes instead of reloading every time
    20x performance gain!

--- js/views/internallocation.js
+++ js/views/internallocation.js
@@ -27,50 +27,56 @@ function drawInternalLocationMap() {
         $("#internalMapTab").show();
         //set global cy defined in dataStructures.js
         //Creating a new cytoscape is a huge time sink!  ~150-250ms each time.
-        cy = cytoscape({
-            container: document.getElementById('internalMapContainer'),
-            
-            style: cytoscape.stylesheet()
-                .selector('node')
-                .css({
-                    'content': 'data(id)',
-                    'shape' : 'roundrectangle'
-                })
-                .selector('edge')
-                .css({
-                    'width': 4,
-                    'line-color': '#ddd',
-                    'target-arrow-color': '#ddd'
-                })
-                .selector('.currentNode')
-                .css({
-                  'background-color': '#51aff0',
-                })
-                .selector('.visitedNode')
-                .css({
-                  'background-color': '#91dffc',
-                })
-                .selector('.unexploredButAdjacentToExplored')
-                .css({
-                  'background-color': '#ddd',
-                })
-                .selector('.unexploredNode')
-                .css({
-                  'display': 'none',
-                }),
-            elements: getInternalEnvironmentMap(getCurrentInternalLocation()),
-            autoungrabify: true,
-            
-            layout: {
-                name: 'preset',
-                directed: false,
-                padding: 10,
-                fit: true,
-                ready: rezoom(), //BE: this doesn't work... 
-            },
-            zoom: 2.5,
-        });
-        rezoom();
+        if (typeof cy === "undefined") {
+            cy = cytoscape({
+                container: document.getElementById('internalMapContainer'),
+                
+                style: cytoscape.stylesheet()
+                    .selector('node')
+                    .css({
+                        'content': 'data(id)',
+                        'shape' : 'roundrectangle'
+                    })
+                    .selector('edge')
+                    .css({
+                        'width': 4,
+                        'line-color': '#ddd',
+                        'target-arrow-color': '#ddd'
+                    })
+                    .selector('.currentNode')
+                    .css({
+                      'background-color': '#51aff0',
+                    })
+                    .selector('.visitedNode')
+                    .css({
+                      'background-color': '#91dffc',
+                    })
+                    .selector('.unexploredButAdjacentToExplored')
+                    .css({
+                      'background-color': '#ddd',
+                    })
+                    .selector('.unexploredNode')
+                    .css({
+                      'display': 'none',
+                    }),
+                elements: getInternalEnvironmentMap(getCurrentInternalLocation()),
+                autoungrabify: true,
+                
+                layout: {
+                    name: 'preset',
+                    directed: false,
+                    padding: 10,
+                    fit: true,
+                    ready: rezoom(), //BE: this doesn't work... 
+                },
+                zoom: 2.5,
+            });
+        } else {
+            cy.remove("node");
+            cy.add(getInternalEnvironmentMap(getCurrentInternalLocation()));
+            cy.fit();
+        }
+        //rezoom(); BE: do this more predictably if it makes sense
         console.log("Cytoscape reload took " + ((new Date()).getTime() - t) + "ms");
     } else {
         $("#internalMapContainer").hide();
